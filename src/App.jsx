76c9921b76e18ad80ee7da6c7657d54fde734cca
pankaj <er.pankaj,k4@gmail.com>
2023-10-17 15:43:30 +0530

    Setting up Redux tool kit using its offical website: creating src\store\store.js.Creating homeSlice in src\store\homeSlice.js. Testing the homeSlice.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
 import {fetchDataFromApi} from "./utils/api";
+import { getApiConfiguration } from './store/homeSlice';
+
 
 function App() {
+  const dispatch = useDispatch();
+// const url = useSelector(state => state.home.url); // or
+const {url} = useSelector(state => state.home);
+
+console.log("url : ", url);
+
   useEffect(() =>{
     apiTesting();
   },[])
+
 const apiTesting =() =>{
   fetchDataFromApi("/movie/popular")
   .then((res) =>{
     console.log(res);
+    dispatch(getApiConfiguration(res));
   })
 }
   return (
-<div className="app">App</div>
+<div className="app">
+{url?.total_pages}
+</div>
   )
 }
 
