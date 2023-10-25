e966a7a98be32c258d5afef6c33ac8a6347ff139
pankaj <er.pankaj,k4@gmail.com>
2023-10-25 23:29:34 +0530

    Detail Page : Creating Recommendation and Similar Section.

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -5,16 +5,22 @@ import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
 import VideosSection from "./videosSection/VideosSection";
+import Recommendation from "./carousels/Recommendation";
+import Similar from "./carousels/Similar";
 
 export const Details = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
-  const { data: credits, loading: creditsLoading } = useFetch( `/${mediaType}/${id}/credits` );
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
-      <Cast data={credits?.cast} loading={creditsLoading}/>
+      <Cast data={credits?.cast} loading={creditsLoading} />
       <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   );
 };
