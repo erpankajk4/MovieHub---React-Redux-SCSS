d75ca9f3fd699767640a4eb1fead2438a13937bf
pankaj <er.pankaj,k4@gmail.com>
2023-10-26 14:50:43 +0530

    Fixing Recommendations not found issue.

--- src/pages/details/carousels/Recommendation.jsx
+++ src/pages/details/carousels/Recommendation.jsx
@@ -8,14 +8,14 @@ const Recommendation = ({ mediaType, id }) => {
     `/${mediaType}/${id}/recommendations`
   );
 
-  return (
+  return data?.results && data?.results.length > 0 ? (
     <Carousel
       title="Recommendations"
       data={data?.results}
       loading={loading}
       endpoint={mediaType}
     />
-  );
+  ) : null;
 };
 
 export default Recommendation;
